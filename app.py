dcd3fff12e65be933d6a31b4032f52066a781952
Mehmet Tarhan <memtarhan@gmail.com>
2020-06-08 12:00:06 +0300

    Updated timer logic

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from Tkinter import *
-import time
+
+height = 1000
+width = 1000
 
 window = Tk()
 
@@ -12,15 +14,17 @@ def countdown(counter):
         # call countdown again after 1000ms (1s)
         window.after(1000, countdown, counter - 1)
     else:
+        timer_label.place(x=height / 2, y=width / 2, anchor="center")
         timer_label["text"] = "GAME OVER"
+        # TODO: Implement Game Over logic
 
 
 def create():
-    window.geometry("600x600")
+    window.geometry("{}x{}".format(height, width))
     window.title("Balloons")
     window.config(background='black')
-    timer_label.config(font=("Symbol", 44, 'bold'), background='black', foreground='red')
-    timer_label.pack()
+    timer_label.config(font=("Helvetica", 64, 'bold'), background='black', foreground='red')
+    timer_label.place(x=width / 2, y=44, anchor="center")
     countdown(5)
     window.mainloop()
 
