5f452a6c5b6a87c792569f3859ed3fde5bbf1c3a
Mehmet Tarhan <memtarhan@gmail.com>
2020-06-08 12:49:02 +0300

    Added score, aim and level labels

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
 from Tkinter import *
 
+timer_counter = 10
+total_aim = 45
+score = 0
+level = 1
 height = 1000
 width = 1000
 
 window = Tk()
 
 timer_label = Label(window, justify='center')
+total_aim_label = Label(window, justify='center')
+score_label = Label(window, justify='center')
+level_label = Label(window, justify='center')
+
+
+def create_countdown():
+    timer_label.config(font=("Helvetica", 64), background='black', foreground='red')
+    timer_label.place(x=width / 2, y=44, anchor="center")
+    countdown(timer_counter)
+
+
+def create_total_aim():
+    total_aim_label.config(font=("Helvetica", 64), background='black', foreground='green')
+    total_aim_label.place(x=width / 2, y=128, anchor="center")
+    total_aim_label["text"] = "Total Aim: {}".format(total_aim)
+
+
+def create_score():
+    score_label.config(font=("Helvetica", 64), background='black', foreground='orange')
+    score_label.place(x=width / 2, y=height - 128, anchor="center")
+    score_label["text"] = "Your score: {}".format(score)
+
+
+def create_level():
+    level_label.config(font=("Helvetica", 64), background='black', foreground='white')
+    level_label.place(x=width / 2, y=height - 44, anchor="center")
+    level_label["text"] = "Your at Level: {}".format(level)
 
 
 def countdown(counter):
@@ -23,9 +54,12 @@ def create():
     window.geometry("{}x{}".format(height, width))
     window.title("Balloons")
     window.config(background='black')
-    timer_label.config(font=("Helvetica", 64, 'bold'), background='black', foreground='red')
-    timer_label.place(x=width / 2, y=44, anchor="center")
-    countdown(5)
+
+    create_countdown()
+    create_total_aim()
+    create_score()
+    create_level()
+
     window.mainloop()
 
 
